7b71d999e74c76f404a93c5a7bd01d70d0776fdc
kikpond <kikpond15@gmail.com>
2025-09-16 12:58:30 +0900

    add project

---
+++ projects/02_heartbeat/src/main.cpp
@@ -0,0 +1,33 @@
+#include <M5Unified.h>
+
+void setup() {
+  auto cfg = M5.config();      // M5Unifiedが機種差を吸収
+  M5.begin(cfg);
+  Serial.begin(115200);
+
+  M5.Display.setTextSize(2);
+  M5.Display.setTextColor(TFT_WHITE, TFT_BLACK);
+  M5.Display.clear();
+
+#ifdef TARGET_M5BASIC
+  M5.Display.println("Board: M5Stack Basic");
+#elif defined(TARGET_M5CORE2)
+  M5.Display.println("Board: M5Stack Core2");
+#endif
+
+
+
+}
+
+void loop() {
+  M5.update();                  // ボタン/タッチ等の状態更新
+  int heatbeat = analogRead(36);
+  //画面に表示
+  String msg = "heatbeat: " + String(heatbeat);
+  M5.Display.drawString(msg, 0, 40);
+
+  //シリアルモニタに表示(VSCodeの場合"Teleplot"で確認可能)
+  Serial.printf("heatbeat:%d\n", heatbeat);
+  delay(10);
+  
+}
