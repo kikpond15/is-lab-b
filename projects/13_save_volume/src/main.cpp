dcdf46f4d73ceff957f6dd65ae13824755a18a05
Katsuya Endoh <studio@enkatsu.org>
2025-10-18 18:28:22 +0900

    update recording sound sample

--- projects/13_save_volume/src/main.cpp
+++ projects/13_save_volume/src/main.cpp
@@ -1,6 +1,6 @@
-#include <M5UnitLCD.h>
-#include <M5UnitOLED.h>
 #include <M5Unified.h>
+#include <SD.h>
+
 static constexpr const size_t record_number = 256;
 static constexpr const size_t record_length = 200;
 static constexpr const size_t record_size = record_number * record_length;
@@ -10,10 +10,14 @@ static int16_t prev_h[record_length];
 static size_t rec_record_idx = 2;
 static size_t draw_record_idx = 0;
 static int16_t *rec_data;
+
 // 1秒間の平均音量計算用
 static constexpr const size_t samples_per_second = record_samplerate / record_length; // 約80回
 static int64_t volume_sum = 0;
 static size_t sample_count = 0;
+
+File sdcard;
+bool loggingState = false; // ログの状態を保持する変数
 
 void setup()
 {
@@ -22,24 +26,34 @@ void setup()
   // cfg.external_speaker.hat_spk2 = true;    /// use external speaker (HAT SPK2)
   // cfg.external_speaker.atomic_spk = true;  /// use external speaker (ATOMIC SPK)
   M5.begin(cfg);
-  M5.Display.startWrite();
-  if (M5.Display.width() > M5.Display.height())
-  {
-    M5.Display.setRotation(M5.Display.getRotation() ^ 1);
-  }
-  M5.Display.setCursor(0, 0);
-  M5.Display.print("REC");
   rec_data = (typeof(rec_data))heap_caps_malloc(record_size * sizeof(int16_t), MALLOC_CAP_8BIT);
   memset(rec_data, 0, record_size * sizeof(int16_t));
   M5.Speaker.setVolume(255);
   /// Since the microphone and speaker cannot be used at the same time, turn off the speaker here.
   M5.Speaker.end();
   M5.Mic.begin();
+
+  SD.begin(4, SPI, 40000000); // SDカード初期化(CSピン4, SPIクロック40MHz)
+  sdcard = SD.open("/m5.csv", FILE_WRITE); // 追記モードでオープン
+  sdcard.println("time,value"); // ヘッダー行書き込み
+  sdcard.flush();
 }
 
 void loop()
 {
   M5.update();
+
+  // ボタンAが押されたらログの開始/停止を切り替え
+  if (M5.BtnA.wasPressed()){
+    if(loggingState){
+      loggingState = false;
+    } else{
+      loggingState = true;
+    }
+  }
+
+  float average_volume = 0;
+
   if (M5.Mic.isEnabled())
   {
     static constexpr int shift = 6;
@@ -58,10 +72,13 @@ void loop()
       // 1秒経過したら平均音量を表示
       if (sample_count >= samples_per_second)
       {
-        float average_volume = (float)volume_sum / (sample_count * record_length);
+        average_volume = (float)volume_sum / (sample_count * record_length);
+
         M5.Display.fillRect(0, 0, M5.Display.width(), 40, TFT_BLACK);
         M5.Display.setCursor(0, 0);
         M5.Display.setTextSize(2);
+        M5.Display.printf(loggingState ? "Recording" : "Stop");
+        M5.Display.setCursor(0, 20);
         M5.Display.printf("Vol: %.0f", average_volume);
         // リセット
         volume_sum = 0;
@@ -99,5 +116,11 @@ void loop()
         rec_record_idx = 0;
       }
     }
+  }
+
+  if (loggingState && sample_count == 0) {
+    String line = String(millis()) + "," + String(average_volume);
+    sdcard.println(line);
+    sdcard.flush();
   }
 }
